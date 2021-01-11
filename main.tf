650ac454a811b2564d634e72e280322413ebe263
Akkamahadevi Hiremath <akkammah626@gmail.com>
2021-01-11 11:16:06 -0700

    ignore id changes to asg attachment

--- main.tf
+++ main.tf
@@ -85,5 +85,8 @@ resource "aws_autoscaling_group" "asg" {
 resource "aws_autoscaling_attachment" "app_tg_attachment" {
   autoscaling_group_name = aws_autoscaling_group.asg.id
   alb_target_group_arn   = var.target_group_arn
+  lifecycle {
+    ignore_changes = [id]
+  }
 }
 
